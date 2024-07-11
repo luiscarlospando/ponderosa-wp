1ab28c89dae4ccfff6292c4916fc71b9401dba35
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-11 17:11:18 -0600

    Dejar de trackear .DS_Store, node_modules, scss y src

--- src/smooth-scrolling.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling
-$('.anchor').on('click', function (event) {
-    event.preventDefault();
-    let Item = $(this).attr('href');
-
-    $('body, html').stop(true, true).animate({
-        scrollTop: $(Item).offset().top - 66
-    }, 1000);
-});
