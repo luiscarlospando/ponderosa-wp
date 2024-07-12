02ad486178b48889735e5f5843d7f21de2513075
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-12 14:20:03 -0600

    Agregar elementos básicos del template

---
+++ footer.php
@@ -0,0 +1,231 @@
+			<section id="bg-aleatorio" class="">
+            <img src="<?php echo esc_url(
+                get_template_directory_uri()
+            ); ?>/assets/images/logo-bg-aleatorio@2x.png" alt="" class="logo img-fluid">
+        </section>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="row">
+                            <div class="col-6 mb-4 mb-lg-0 col-lg-3 text-lg-center border-lg-end">
+                                <h1>Título 1</h1>
+                                <nav>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <div class="col-6 mb-4 mb-lg-0 col-lg-3 text-lg-center border-lg-end">
+                                <h1>Título 1</h1>
+                                <nav>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <div class="col-6 mb-4 mb-lg-0 col-lg-3 text-lg-center border-lg-end">
+                                <h1>Título 1</h1>
+                                <nav>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                            <div class="col-6 mb-4 mb-lg-0 col-lg-3 text-lg-center border-lg-end">
+                                <h1>Título 1</h1>
+                                <nav>
+                                    <ul class="list-unstyled">
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                Elemento
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 offset-lg-1">
+                        <p class="text-uppercase fw-bold">Síguenos en nuestras redes</p>
+                        <ul class="social list-inline">
+                            <li class="list-inline-item">
+                                <a href="https://" target="_blank">
+                                    <i class="fa-brands fa-instagram"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://" target="_blank">
+                                    <i class="fa-brands fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="https://" target="_blank">
+                                    <i class="fa-brands fa-tiktok"></i>
+                                </a>
+                            </li>
+                        </ul>
+
+                        <ul class="list-unstyled">
+                            <li>
+                                <address>
+                                    <i class="fa-solid fa-location-pin"></i> José Santos Chocano 970<br>
+                                    San Nicolás de los Garza, N.L. México
+                                </address>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-headset"></i> <a href="tel:+528181581717">81 8158 1717</a>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-envelope"></i> <a href="mailto:ponderosa@bafar.com.mx">ponderosa@bafar.com.mx</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row my-4">
+                    <div class="col">
+                        <hr>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <div class="col-12 mb-4">
+                        <a href="/">
+                            <img src="<?php echo esc_url(
+                                get_template_directory_uri()
+                            ); ?>/assets/images/logo-rojo@2x.png" alt="" class="logo img-fluid">
+                        </a>
+                    </div>
+                    <div class="col-12">
+                        <p class="copyright-text">© <?php echo date(
+                            "Y"
+                        ); ?> <a href="https://grupobafar.com/" target="_blank"">Grupo Bafar</a>. Todos los derechos reservados.</p >
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- a href="https://wa.me/52614?text=Hola%20FOO%20BAR,%20necesito%20información." class="whatsapp" target="_blank">
+            <i class="fab fa-whatsapp whatsapp-icon"></i>
+        </a -->
+
+        <?php wp_footer(); ?>
+
+        <!-- script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script -->
+        <script src="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/assets/js/app.bundle.js"></script>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html lang="es-MX">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="<?php bloginfo("description"); ?>">
+        <title><?php
+        wp_title("");
+        if (wp_title("", false)) {
+            echo " : ";
+        }
+        bloginfo("name");
+        ?></title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/site.webmanifest">
+        <link rel="mask-icon" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#e41a2f">
+        <meta name="theme-color" content="#e41a2f">
+
+        <link rel="stylesheet" href="<?php echo esc_url(
+            get_template_directory_uri()
+        ); ?>/assets/css/styles.css" />
+
+        <?php wp_head(); ?>
+    </head>
+    <body class="reddit-sans-regular">
+        <div id="backdrop"></div>
+        <div class="menu">
+            <a id="cerrar-menu" href="javascript:void(0)">
+                <i class="fas fa-times"></i>
+            </a>
+            <div class="menu-contenido">
+                <a class="anchor" id="btn-logo" href="<?php echo esc_url(
+                    home_url()
+                ); ?>">
+                    <img
+                        class="logo img-fluid"
+                        src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/logo-rojo@2x.png"
+                        alt=""
+                    />
+                </a>
+                <nav>
+                    <ul id="navmenu" class="list-unstyled">
+                        <li>
+                            <a class="anchor" id="btn-nav-1" href="#"
+                                >Productos</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-2" href="#"
+                                >Nuestra Historia</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-3" href="#"
+                                >Recetas</a
+                            >
+                        </li>
+                        <li>
+                            <a class="anchor" id="btn-nav-4" href="#"
+                                >Consejos Prácticos</a
+                            >
+                        </li>
+                    </ul>
+                </nav>
+                <a href="#" class="anchor btn btn-primary" id="btn-contacto"
+                    >Contáctanos</a
+                >
+                <div id="social">
+                    <ul class="list-inline">
+                        <li class="list-inline-item">
+                            <a href="https://" target="_blank">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://" target="_blank">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="https://" target="_blank">
+                                <i class="fab fa-tiktok"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- div id="contacto-menu">
+                    <ul class="list-unstyled">
+                        <li>
+                            <i class="far fa-envelope"></i>
+                            <a href="mailto:cmail@domain.com"
+                                >mail@domain.com</a
+                            >
+                        </li>
+                        <li>
+                            <i class="fas fa-phone"></i>
+                            <a href="tel:+526141234567">(614) 123 4567</a>
+                        </li>
+                    </ul>
+                </div -->
+            </div>
+        </div>
+
+        <header id="navbar">
+            <div class="container">
+                <div class="row">
+                    <div class="d-none d-lg-block col-lg-5 my-auto text-center">
+                        <nav>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <a href="">Productos</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="">Nuestra Historia</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="">Recetas</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-6 col-lg-2 text-lg-center">
+                        <a href="<?php echo esc_url(home_url()); ?>">
+                            <img
+                                src="<?php echo esc_url(
+                                    get_template_directory_uri()
+                                ); ?>/assets/images/logo@2x.png"
+                                alt=""
+                                class="logo img-fluid"
+                                id="logo-navbar"
+                            />
+                        </a>
+                    </div>
+                    <div class="col-lg-5 d-none d-lg-block my-auto text-center">
+                        <nav>
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <a href="">Consejos Prácticos</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="">Contacto</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <ul class="list-inline">
+                                        <li class="list-inline-item">
+                                            Síguenos
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <ul class="list-inline list-inline-nested">
+                                                <li class="list-inline-item">
+                                                    <a
+                                                        href="https://"
+                                                        alt="Instagram"
+                                                        target="_blank"
+                                                    >
+                                                        <i
+                                                            class="fa-brands fa-instagram"
+                                                        ></i>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <a
+                                                        href="https://"
+                                                        alt="Facebook"
+                                                        target="_blank"
+                                                    >
+                                                        <i class="fa-brands fa-facebook"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="list-inline-item">
+                                                    <a
+                                                        href="https://"
+                                                        alt="TikTok"
+                                                        target="_blank"
+                                                    >
+                                                        <i
+                                                            class="fa-brands fa-tiktok"
+                                                        ></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-6 d-lg-none my-auto text-end">
+                        <a
+                            id="mburger"
+                            class="d-lg-none"
+                            href="javascript:void(0)"
+                        >
+                            <i class="fas fa-bars"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </header>
