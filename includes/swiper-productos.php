9fac2d76b9ca9c2dbfbf26a5e7e957ed8275329f
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-15 12:50:29 -0600

    Incluir slider de productos en donde se vaya requiriendo

---
+++ includes/swiper-productos.php
@@ -0,0 +1,110 @@
+<!-- Swiper -->
+<div class="swiper swiper-productos">
+    <div class="swiper-pagination"></div>
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <div class="card">
+                <a href="<?php echo get_category_link(5); ?>">
+                    <img src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/thumb-embutidos.png" class="card-img-top img-fluid" alt="">
+                </a>
+                <div class="card-body">
+                    <h1 class="card-title">Embutidos</h1>
+                    <a href="<?php echo get_category_link(
+                        5
+                    ); ?>" class="btn btn-primary">
+                        Ver productos
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="card">
+                <a href="<?php echo get_category_link(4); ?>">
+                    <img src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/thumb-salchichas.png" class="card-img-top img-fluid" alt="">
+                </a>
+                <div class="card-body">
+                    <h1 class="card-title">Salchichas</h1>
+                    <a href="<?php echo get_category_link(
+                        4
+                    ); ?>" class="btn btn-primary">
+                        Ver productos
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="card">
+                <a href="<?php echo get_category_link(6); ?>">
+                    <img src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/thumb-quesos.png" class="card-img-top img-fluid" alt="">
+                </a>
+                <div class="card-body">
+                    <h1 class="card-title">Quesos</h1>
+                    <a href="<?php echo get_category_link(
+                        6
+                    ); ?>" class="btn btn-primary">
+                        Ver productos
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="card">
+                <a href="<?php echo get_category_link(5); ?>">
+                    <img src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/thumb-embutidos.png" class="card-img-top img-fluid" alt="">
+                </a>
+                <div class="card-body">
+                    <h1 class="card-title">Embutidos</h1>
+                    <a href="<?php echo get_category_link(
+                        5
+                    ); ?>" class="btn btn-primary">
+                        Ver productos
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="card">
+                <a href="<?php echo get_category_link(4); ?>">
+                    <img src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/thumb-salchichas.png" class="card-img-top img-fluid" alt="">
+                </a>
+                <div class="card-body">
+                    <h1 class="card-title">Salchichas</h1>
+                    <a href="<?php echo get_category_link(
+                        4
+                    ); ?>" class="btn btn-primary">
+                        Ver productos
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="swiper-slide">
+            <div class="card">
+                <a href="<?php echo get_category_link(6); ?>">
+                    <img src="<?php echo esc_url(
+                        get_template_directory_uri()
+                    ); ?>/assets/images/thumb-quesos.png" class="card-img-top img-fluid" alt="">
+                </a>
+                <div class="card-body">
+                    <h1 class="card-title">Quesos</h1>
+                    <a href="<?php echo get_category_link(
+                        6
+                    ); ?>" class="btn btn-primary">
+                        Ver productos
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+</div>
--- single-receta.php
+++ single-receta.php
@@ -209,65 +209,14 @@
                 </div>
                 <div class="row">
                     <div class="col">
-                        <!-- Swiper -->
-                        <div class="swiper swiper-productos">
-                            <div class="swiper-pagination"></div>
-                            <div class="swiper-wrapper">
-                                <div class="swiper-slide">
-                                    <div class="card">
-                                        <a href="<?php echo get_category_link(
-                                            5
-                                        ); ?>">
-                                            <img src="<?php echo esc_url(
-                                                get_template_directory_uri()
-                                            ); ?>/assets/images/thumb-embutidos.png" class="card-img-top img-fluid" alt="">
-                                        </a>
-                                        <div class="card-body">
-                                            <h1 class="card-title">Embutidos</h1 >
-                                            <a href="#" class="btn btn-primary">
-                                                Ver productos
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="card">
-                                        <a href="<?php echo get_category_link(
-                                            4
-                                        ); ?>">
-                                            <img src="<?php echo esc_url(
-                                                get_template_directory_uri()
-                                            ); ?>/assets/images/thumb-salchichas.png" class="card-img-top img-fluid" alt="">
-                                        </a>
-                                        <div class="card-body">
-                                            <h1 class="card-title">Salchichas</h1 >
-                                            <a href="#" class="btn btn-primary">
-                                                Ver productos
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="card">
-                                        <a href="<?php echo get_category_link(
-                                            6
-                                        ); ?>">
-                                            <img src="<?php echo esc_url(
-                                                get_template_directory_uri()
-                                            ); ?>/assets/images/thumb-quesos.png" class="card-img-top img-fluid" alt="">
-                                        </a>
-                                        <div class="card-body">
-                                            <h1 class="card-title">Quesos</h1 >
-                                            <a href="#" class="btn btn-primary">
-                                                Ver productos
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="swiper-button-next"></div>
-                            <div class="swiper-button-prev"></div>
-                        </div>
+                        <?php
+                        $swiperProductos = locate_template(
+                            "./assets/includes/swiper-productos.php"
+                        );
+                        if ($swiperProductos) {
+                            load_template($swiperProductos, true);
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
