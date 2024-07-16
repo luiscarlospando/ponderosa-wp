cd61db310af9c93c9d4dd9b736867a4ecff3701a
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-15 23:34:38 -0600

    Agregar template de la página recetas

---
+++ page-recetas.php
@@ -0,0 +1,179 @@
+<?php
+/*
+    Template Name: Recetas
+    */
+
+get_header(); ?>
+
+    <section id="jumbotron" style="background: url('<?php echo esc_url(
+        get_the_post_thumbnail_url(get_the_ID(), "full")
+    ); ?>') no-repeat; background-size: cover; background-position: center;">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>
+                        <strong>Ponderosa:</strong><br>
+                        creando <strong>recetas</strong><br>
+                        para la <strong>familia</strong>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section  class="recetas-generico section-red py-60">
+        <div class="overlay"></div>
+        <div class="container-fluid">
+            <div class="row mb-5">
+                <div class="col-lg-10 offset-lg-1 text-center">
+                    <h1>Recetas de temporada</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <a href="#" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section  class="recetas-generico section-white py-60">
+        <div class="overlay"></div>
+        <div class="container-fluid">
+            <div class="row mb-5">
+                <div class="col-lg-10 offset-lg-1 text-center">
+                    <h1>Recetas pa'la carnita asada</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <a href="#" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="consejos" class="section-white">
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col-lg-10 offset-lg-1 text-center">
+                    <h1>Consejos Útiles</h1>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates ea voluptatum error eius, hic ullam optio impedit reiciendis doloremque nihil molestias eligendi ratione sint harum architecto culpa veniam cum accusantium!</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <?php
+                    $swiperConsejos = locate_template(
+                        "includes/swiper-consejos.php"
+                    );
+                    if ($swiperConsejos) {
+                        load_template($swiperConsejos, true);
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="productos" class="section-bg-salchichas py-60">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row mb-5">
+                <div class="col">
+                    <h1>Productos</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <?php
+                    $swiperProductos = locate_template(
+                        "includes/swiper-productos.php"
+                    );
+                    if ($swiperProductos) {
+                        load_template($swiperProductos, true);
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
