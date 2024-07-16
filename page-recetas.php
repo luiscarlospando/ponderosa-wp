53292b21e7cc8e974b472d0545986c24542ed9ba
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-16 17:29:53 -0600

    Incluir loop de recetas de temporada con offset

--- page-recetas.php
+++ page-recetas.php
@@ -96,7 +96,7 @@ get_header(); ?>
             </div>
             <div class="row">
                 <div class="col text-center">
-                    <a href="#" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-pa-la-carnita-asada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
                 </div>
             </div>
         </div>
@@ -111,46 +111,18 @@ get_header(); ?>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="<?php echo esc_url(
-                            get_template_directory_uri()
-                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
-                        <div class="card-body">
-                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
-                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="<?php echo esc_url(
-                            get_template_directory_uri()
-                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
-                        <div class="card-body">
-                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
-                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="<?php echo esc_url(
-                            get_template_directory_uri()
-                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
-                        <div class="card-body">
-                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
-                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                $loopRecetasDeTemporadaOffset3 = locate_template(
+                    "includes/loop-recetas-de-temporada-offset-3.php"
+                );
+                if ($loopRecetasDeTemporadaOffset3) {
+                    load_template($loopRecetasDeTemporadaOffset3, true);
+                }
+                ?>
             </div>
             <div class="row">
                 <div class="col text-center">
-                    <a href="#" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
                 </div>
             </div>
         </div>
