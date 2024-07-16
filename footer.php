6c86874788b625ea6afaf26ee5e24fd16351bcdc
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-15 23:20:52 -0600

    Agregar smooth scroll al cargar página Nuestra Historia

--- footer.php
+++ footer.php
@@ -245,6 +245,17 @@
             </script>
         <?php endif; ?>
 
+        <?php if (is_page(2)): ?>
+            <script>
+                document.addEventListener("DOMContentLoaded", function() {
+                    const anchor = document.getElementById("historia");
+                    if (anchor) {
+                        anchor.scrollIntoView({ behavior: 'smooth' });
+                    }
+                });
+            </script>
+        <?php endif; ?>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
         <script src="<?php echo esc_url(
