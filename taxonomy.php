e6b154ae1d00cc66de54b07ba22f8cc61187652f
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-15 23:04:30 -0600

    Quitar swiper

--- taxonomy.php
+++ taxonomy.php
@@ -15,45 +15,31 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    <!-- Swiper -->
-                    <div class="swiper swiper-productos">
-                        <div class="swiper-pagination"></div>
-                        <div class="swiper-wrapper">
-                            <?php if (have_posts()):
-                                while (have_posts()):
-                                    the_post(); ?>
-                                <div class="swiper-slide">
-                                    <div class="card">
-                                        <a href="<?php the_permalink(); ?>">
-                                            <?php the_post_thumbnail(
-                                                "thumb-vertical",
-                                                [
-                                                    "class" =>
-                                                        "card-img-top img-fluid",
-                                                ]
-                                            ); ?>
-                                        </a>
-                                        <div class="card-body">
-                                            <h1 class="card-title"><?php the_title(); ?></h1 >
-                                            <a href="<?php the_permalink(); ?>" class="btn btn-primary">
-                                                Ver producto
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php
-                                endwhile; ?>
-                            <?php
-                            else:
-                                 ?>
-                            <?php
-                            endif; ?>
+                <?php if (have_posts()):
+                    while (have_posts()):
+                        the_post(); ?>
+                    <div class="col-lg-4 mb-5">
+                        <div class="card">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail("thumb-vertical", [
+                                    "class" => "card-img-top img-fluid",
+                                ]); ?>
+                            </a>
+                            <div class="card-body">
+                                <h1 class="card-title"><?php the_title(); ?></h1 >
+                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">
+                                    Ver producto
+                                </a>
+                            </div>
                         </div>
-                        <div class="swiper-button-next"></div>
-                        <div class="swiper-button-prev"></div>
                     </div>
-                </div>
+                <?php
+                    endwhile; ?>
+                <?php
+                else:
+                     ?>
+                <?php
+                endif; ?>
             </div>
         </div>
     </section>
