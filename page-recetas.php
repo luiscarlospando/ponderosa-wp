62a7b4cb07a8f2e08329e9a52a1406fbe4bf59e1
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-15 23:40:41 -0600

    Agregar sección faltante

--- page-recetas.php
+++ page-recetas.php
@@ -130,6 +130,60 @@ get_header(); ?>
         </div>
     </section>
 
+    <section  class="recetas-generico section-red py-60">
+        <div class="overlay"></div>
+        <div class="container-fluid">
+            <div class="row mb-5">
+                <div class="col-lg-10 offset-lg-1 text-center">
+                    <h1>Recetas de temporada</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-4">
+                    <div class="card">
+                        <img src="<?php echo esc_url(
+                            get_template_directory_uri()
+                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
+                        <div class="card-body">
+                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
+                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <a href="#" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <section id="consejos" class="section-white">
         <div class="container">
             <div class="row mb-5">
