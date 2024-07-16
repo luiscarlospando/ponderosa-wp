e0302631db0ae0ae00099bb4bbfb2c0cd8841b45
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-16 17:12:06 -0600

    Incluir loop de recetas de temporada

--- page-recetas.php
+++ page-recetas.php
@@ -31,42 +31,14 @@ get_header(); ?>
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="<?php echo esc_url(
-                            get_template_directory_uri()
-                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
-                        <div class="card-body">
-                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
-                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="<?php echo esc_url(
-                            get_template_directory_uri()
-                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
-                        <div class="card-body">
-                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
-                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 mb-4">
-                    <div class="card">
-                        <img src="<?php echo esc_url(
-                            get_template_directory_uri()
-                        ); ?>/assets/images/thumb-receta.png" class="card-img-left img-fluid" alt="">
-                        <div class="card-body">
-                            <h1 class="card-title">Quesadillas con queso y chorizo Ponderosa</h1>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ut magnam perferendis mollitia dolores odit distinctio ea a corporis?</p>
-                            <a href="#" class="btn btn-primary">Ver receta <i class="fa-solid fa-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
+                <?php
+                $loopRecetasDeTemporada = locate_template(
+                    "includes/loop-recetas-de-temporada.php"
+                );
+                if ($loopRecetasDeTemporada) {
+                    load_template($loopRecetasDeTemporada, true);
+                }
+                ?>
             </div>
             <div class="row">
                 <div class="col text-center">
