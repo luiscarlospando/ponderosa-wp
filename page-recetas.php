a87c7d16832df22d1eb4a61190ba455f6dec21eb
Luis Carlos Pando <hey@luiscarlospando.com>
2024-07-23 16:20:49 -0600

    Comentar jumbotron

--- page-recetas.php
+++ page-recetas.php
@@ -5,7 +5,7 @@
 
 get_header(); ?>
 
-    <section id="jumbotron" style="background: url('<?php echo esc_url(
+    <!-- section id="jumbotron" style="background: url('<?php echo esc_url(
         get_the_post_thumbnail_url(get_the_ID(), "full")
     ); ?>') no-repeat; background-size: cover; background-position: center;">
         <div class="overlay"></div>
@@ -20,104 +20,7 @@ get_header(); ?>
                 </div>
             </div>
         </div>
-    </section>
-
-    <section id="recetas">
-        <div class="container-fluid">
-            <div class="row g-0">
-                <div class="col-lg-4 my-auto">
-                    <section  class="recetas-generico section-red py-60">
-                        <div class="overlay"></div>
-                        <div class="container-fluid">
-                            <div class="row mb-5">
-                                <div class="col-lg-10 offset-lg-1 text-center">
-                                    <h1>Recetas de temporada</h1>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <?php
-                                $loopRecetasDeTemporada = locate_template(
-                                    "includes/loop-recetas-de-temporada.php"
-                                );
-                                if ($loopRecetasDeTemporada) {
-                                    load_template(
-                                        $loopRecetasDeTemporada,
-                                        true
-                                    );
-                                }
-                                ?>
-                            </div>
-                            <div class="row">
-                                <div class="col text-center">
-                                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-                <div class="col-lg-4 my-auto">
-                    <section  class="recetas-generico section-white py-60">
-                        <div class="overlay"></div>
-                        <div class="container-fluid">
-                            <div class="row mb-5">
-                                <div class="col-lg-10 offset-lg-1 text-center">
-                                    <h1>Recetas pa' la carnita asada</h1>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <?php
-                                $loopRecetasPaLaCarnitaAsada = locate_template(
-                                    "includes/loop-recetas-pa-la-carnita-asada.php"
-                                );
-                                if ($loopRecetasPaLaCarnitaAsada) {
-                                    load_template(
-                                        $loopRecetasPaLaCarnitaAsada,
-                                        true
-                                    );
-                                }
-                                ?>
-                            </div>
-                            <div class="row">
-                                <div class="col text-center">
-                                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-pa-la-carnita-asada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-                <div class="col-lg-4 my-auto">
-                    <section  class="recetas-generico section-red py-60">
-                        <div class="overlay"></div>
-                        <div class="container-fluid">
-                            <div class="row mb-5">
-                                <div class="col-lg-10 offset-lg-1 text-center">
-                                    <h1>Recetas de temporada</h1>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <?php
-                                $loopRecetasDeTemporadaOffset3 = locate_template(
-                                    "includes/loop-recetas-de-temporada-offset-3.php"
-                                );
-                                if ($loopRecetasDeTemporadaOffset3) {
-                                    load_template(
-                                        $loopRecetasDeTemporadaOffset3,
-                                        true
-                                    );
-                                }
-                                ?>
-                            </div>
-                            <div class="row">
-                                <div class="col text-center">
-                                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </div>
-    </section>
+    </section -->
 
     <section  class="recetas-generico section-red py-60">
         <div class="overlay"></div>
