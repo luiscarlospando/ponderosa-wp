9b0fd8787933beefdca482e88776fa3ddf42d8a6
Luis Carlos Pando <hey@luiscarlospando.com>
2024-08-07 16:51:14 -0600

    Modificar estructura de las recetas

--- page-recetas.php
+++ page-recetas.php
@@ -26,75 +26,71 @@ get_header(); ?>
         <div class="overlay"></div>
         <div class="container-fluid">
             <div class="row mb-5">
-                <div class="col-lg-10 offset-lg-1 text-center">
-                    <h1>Recetas de temporada</h1>
+                <div class="col-lg-4">
+                    <div class="row mb-5">
+                        <div class="col-12 text-center">
+                            <h1>Recetas de temporada</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $loopRecetasDeTemporada = locate_template(
+                            "includes/loop-recetas-de-temporada.php"
+                        );
+                        if ($loopRecetasDeTemporada) {
+                            load_template($loopRecetasDeTemporada, true);
+                        }
+                        ?>
+                    </div>
+                    <div class="row">
+                        <div class="col text-center">
+                            <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <?php
-                $loopRecetasDeTemporada = locate_template(
-                    "includes/loop-recetas-de-temporada.php"
-                );
-                if ($loopRecetasDeTemporada) {
-                    load_template($loopRecetasDeTemporada, true);
-                }
-                ?>
-            </div>
-            <div class="row">
-                <div class="col text-center">
-                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                <div class="col-lg-4">
+                    <div class="row mb-5">
+                        <div class="col-12 text-center">
+                            <h1>Recetas pa' la carnita asada</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $loopRecetasPaLaCarnitaAsada = locate_template(
+                            "includes/loop-recetas-pa-la-carnita-asada.php"
+                        );
+                        if ($loopRecetasPaLaCarnitaAsada) {
+                            load_template($loopRecetasPaLaCarnitaAsada, true);
+                        }
+                        ?>
+                    </div>
+                    <div class="row">
+                        <div class="col text-center">
+                            <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </section>
-
-    <section  class="recetas-generico section-white py-60">
-        <div class="overlay"></div>
-        <div class="container-fluid">
-            <div class="row mb-5">
-                <div class="col-lg-10 offset-lg-1 text-center">
-                    <h1>Recetas pa' la carnita asada</h1>
-                </div>
-            </div>
-            <div class="row">
-                <?php
-                $loopRecetasPaLaCarnitaAsada = locate_template(
-                    "includes/loop-recetas-pa-la-carnita-asada.php"
-                );
-                if ($loopRecetasPaLaCarnitaAsada) {
-                    load_template($loopRecetasPaLaCarnitaAsada, true);
-                }
-                ?>
-            </div>
-            <div class="row">
-                <div class="col text-center">
-                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-pa-la-carnita-asada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section  class="recetas-generico section-red py-60">
-        <div class="overlay"></div>
-        <div class="container-fluid">
-            <div class="row mb-5">
-                <div class="col-lg-10 offset-lg-1 text-center">
-                    <h1>Recetas de temporada</h1>
-                </div>
-            </div>
-            <div class="row">
-                <?php
-                $loopRecetasDeTemporadaOffset3 = locate_template(
-                    "includes/loop-recetas-de-temporada-offset-3.php"
-                );
-                if ($loopRecetasDeTemporadaOffset3) {
-                    load_template($loopRecetasDeTemporadaOffset3, true);
-                }
-                ?>
-            </div>
-            <div class="row">
-                <div class="col text-center">
-                    <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                <div class="col-lg-4">
+                    <div class="row mb-5">
+                        <div class="col-12 text-center">
+                            <h1>Recetas de temporada</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <?php
+                        $loopRecetasDeTemporadaOffset3 = locate_template(
+                            "includes/loop-recetas-de-temporada-3.php"
+                        );
+                        if ($loopRecetasDeTemporadaOffset3) {
+                            load_template($loopRecetasDeTemporadaOffset3, true);
+                        }
+                        ?>
+                    </div>
+                    <div class="row">
+                        <div class="col text-center">
+                            <a href="<?php echo site_url(); ?>/categoria-recetas/recetas-de-temporada/" class="btn btn-primary">Ver más <i class="fa-solid fa-arrow-right"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
