1df54f6d312dbcff710480c9cb989117a53860d9
Luis Carlos Pando <hey@luiscarlospando.com>
2024-11-05 18:06:17 -0600

    Reestructurar vista productos

--- single-producto.php
+++ single-producto.php
@@ -68,7 +68,7 @@
                                 ?>
 
                                 <div class="row">
-                                    <div class="col-6">
+                                    <div class="col-md-3">
                                         <?php if (have_rows("presentacion")): ?>
                                             <h2>Presentación</h2>
                                             <?php while (
@@ -103,7 +103,7 @@
                                             endif; ?>
                                         </ul>
                                     </div>
-                                    <div class="col-6">
+                                    <div class="col-md-9">
                                         <ul class="list-inline especificaciones">
                                             <?php if (get_field("porcion")): ?>
                                                 <li class="list-inline-item">
