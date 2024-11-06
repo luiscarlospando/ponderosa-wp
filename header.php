bbe42a5501a2f868096f545713cda621fe965948
Luis Carlos Pando <hey@luiscarlospando.com>
2024-11-06 15:39:41 -0600

    Cambios solicitados por cliente

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 
         <link rel="stylesheet" href="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/css/styles.css?v=12" />
+        ); ?>/assets/css/styles.css?v=13" />
 
         <?php wp_head(); ?>
     </head>
