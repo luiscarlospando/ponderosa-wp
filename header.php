839029afa6d2acff541bc7cf8da272848ac85478
Luis Carlos Pando <hey@luiscarlospando.com>
2024-11-05 18:21:16 -0600

    Reestructurar vista productos

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 
         <link rel="stylesheet" href="<?php echo esc_url(
             get_template_directory_uri()
-        ); ?>/assets/css/styles.css?v=4" />
+        ); ?>/assets/css/styles.css?v=5" />
 
         <?php wp_head(); ?>
     </head>
